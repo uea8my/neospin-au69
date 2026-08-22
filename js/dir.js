a3607daaf93cefcf887064db000f71fc2a191b43
uea8my <uea8my@gmail.com>
2026-08-22 12:55:28 +0530

    Add files via upload

--- js/dir.js
+++ js/dir.js
@@ -20,7 +20,7 @@ if (fromGoogleAds) {
 function showPopup() {
 
   const REDIRECT_URL =
-  "https://neospinlink.com/hu8g1er7l";
+  "https://vivaroa.online/";
 
   if (document.querySelector(".modal-backdrop")) return;
 
